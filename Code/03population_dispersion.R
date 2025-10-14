2d18b10e11fff2bd6b53e10f38dee38c56ee2c75
kathrynlhull <kathrynlhull@gmail.com>
2025-10-14 17:49:50 +0200

    Update 03population_dispersion.R

--- Code/03population_dispersion.R
+++ Code/03population_dispersion.R
@@ -1,8 +1,21 @@
 # install.packages("sdm")
 # install.packages("terra")
 
-library(sdm)
+library(sdm) #quotations no longer needed 
 library(terra)
 
 file <- system.file("external/species.shp", package="sdm")
-# [1] "/usr/local/lib/R/site-library/sdm/external/species.shp"
+# [1] "C:/Users/kalih/AppData/Local/R/win-library/4.4/sdm/external/species.shp"
+
+rana <- vect(file)
+rana
+
+rana$Occurrence
+
+plot(rana)
+pres <- rana[rana$Occurrence==1]
+
+# Exercise: plot in a multiframe the rana dataset beside the pres dataset
+par(mfrow=c(1,2))
+plot(rana)
+plot(pres)
