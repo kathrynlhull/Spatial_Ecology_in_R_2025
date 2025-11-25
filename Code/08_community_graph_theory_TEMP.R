37f986a25adb514c04f2184cb2bfb977defd22c4
kathrynlhull <kathrynlhull@gmail.com>
2025-11-25 11:25:08 +0100

    Update 08_community_graph_theory_TEMP.R

--- Code/08_community_graph_theory_TEMP.R
+++ Code/08_community_graph_theory_TEMP.R
@@ -3,13 +3,23 @@ library(igraph)
 
 species <- c("Algae", "Zooplankton", "Small Fish", "Large Fish", "Bird")
 
-interactions <- data.frame(
-  predator = c("Zooplankton", "Small Fish", "Large Fish", "Bird", "Bird"),
-  prey     = c("Algae", "Zooplankton", "Small Fish", "Small fish", "Large Fish")
-)
+predator <- c("Zooplankton", "Small Fish", "Large Fish", "Bird", "Bird")
+prey <- c("Algae", "Zooplankton", "Small Fish", "Small Fish", "Large Fish")
 
-# Create graph
-g <- graph_from_data_frame(interactions, vertices = species, directed = TRUE)
+#in the prey and predator groups the order is not casual, they are paired, so each prey predator correspond to a prey (e.g. the zooplankton is hte predator of the algae etc.)
+
+interactions <- data.frame(predator, prey)
+#This builds a table, a dataset with the predator prey interactions 
+
+g <- graph_from_data_frame(interactions, vertices = species, directed = T)
+#the vertices arguments specifies which are the points that are going to appear in the graph (in our case they will be the predators and preys)
+#directed means that there is a direction from the predators to the preys
+plot(g)
+
+#We are going to use a function to produce an igraph from a table 
+g <- graph_from_data_frame(interactions, vertices = species, directed = F)
+#without directions
+plot(g)
 
 # Plot with clear labels
 plot(
@@ -26,3 +36,9 @@ plot(
 # Stats
 cat("\nNumber of species (nodes):", vcount(g))
 cat("\nNumber of interactions (edges):", ecount(g))
+
+# set.seed() function
+#the set.seed function selects one of the possible graphs that we can obtain.  Set.seed (42), the number is chosen at random. 
+set.seed(42)
+g <- graph_from_data_frame(interactions, vertices = species, directed = T)
+plot(g)
