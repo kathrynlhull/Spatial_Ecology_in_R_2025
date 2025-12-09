4df512c9242e3035b660e834b79238699e7c0efd
kathrynlhull <kathrynlhull@gmail.com>
2025-12-09 17:45:45 +0100

    Update 14_classification_TEMP.R

--- Code/14_classification_TEMP.R
+++ Code/14_classification_TEMP.R
@@ -8,25 +8,14 @@ library(patchwork)
 
 im.list()
 
+sun <- im.import("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
+
+sunc <- im.classify(sun, num_clusters=3)
+
+#--- Mato Grosso example
+
 m1992 <- im.import("matogrosso_l5_1992219_lrg.jpg")
-#layers: 1=NIR, 2=red, 3=green
-
-plot(m1992) #rgb123
-
-sun<-im.import("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
-plot(sun)
-
-?im.classify
-
-sunc<-im.classify(sun,num_clusters=3) #unsupervised classification 
-par(mfrow=c(2,1))
-plot(sun)
-plot(sunc)
-
-
-#Excercise: import the image from 2006 
 m2006 <- im.import("matogrosso_ast_2006209_lrg.jpg")
-plot(m2006)
 
 m1992c <- im.classify(m1992, num_clusters=2)
 # class 1 = human related areas and water
@@ -51,7 +40,7 @@ p2006 = f2006 * 100 / tot2006
 # class 1 = forest = 45
 # class 2 = human related areas and water = 55
 
-# building a dataframe
+# building a dataframe with 3 columns (class, perc1992, perc2006)
 class <- c("Forest","Human")
 y1992 <- c(83, 17)
 y2006 <- c(45, 55)
@@ -59,8 +48,14 @@ y2006 <- c(45, 55)
 tabout <- data.frame(class, y1992, y2006)
 tabout
 
-# final graph
+# final graph using ggplot2 package.  Aes means aesthetics
+p1 <- ggplot(tabout, aes(x=class, y=perc1992, color=class)) +
+geom_bar(stat="identity", fill="white")
+
+#coding below uses PATCHWORK package instead of parmfrow function to plot two graphs side by side 
+#Ylim sets the scale which MUST BE THE SAME FOR BOTH PLOTS 
+
 p1 <- ggplot(tabout, aes(x=class, y=y1992, color=class)) + geom_bar(stat="identity", fill="white") + ylim(c(0,100))
 p2 <- ggplot(tabout, aes(x=class, y=y2006, color=class)) + geom_bar(stat="identity", fill="white") + ylim(c(0,100))
-p1  + p2
+p1  + p2alle
 p1 / p2
